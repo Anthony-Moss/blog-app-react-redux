90846b0fc8ac8b69b28ecdb974ee3dcb5c160426
Anthony Moss <asmoss93@gmail.com>
2019-05-09 14:08:20 -0400

    adds smart and dumb blog detail

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import BlogList from './containers/BlogListContainer.js';
-
+import BlogDetail from './containers/BlogDetailContainer';
 function App() {
   return (
     <div className="App">
       <BlogList />
+      <BlogDetail />
     </div>
   );
 }
