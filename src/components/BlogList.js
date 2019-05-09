5b2b123376c44dbe648df8f5e323977e99352f4b
Anthony Moss <asmoss93@gmail.com>
2019-05-09 11:59:57 -0400

    corrected spelling for BlogList function

--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function BlogList({posts}) {
     // Helper function implicitly returns <li>
     const listItems = Object.values(posts).map(id => {
-        const theBlogPost = post[id];
+        const theBlogPost = posts[id];
         return <li>{theBlogPost.title} - {id}</li>
     });
     return (
