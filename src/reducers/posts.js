a09558c4e7d4c1731bbe059e56b63a85fba26920
Anthony Moss <asmoss93@gmail.com>
2019-05-09 10:34:32 -0400

    adds posts in reducer with a random id

--- src/reducers/posts.js
+++ src/reducers/posts.js
@@ -2,6 +2,8 @@
 import {
     ACTION_CREATE_POST
 } from '../actions';
+
+import generateId from '../utils';
 // A reducer is a function that accepts
 // the current state  and an action then
 // calculates/returns the next new
@@ -11,7 +13,17 @@ export default function posts(state={}, action={type: ''}) {
     switch(action.type) {
         case ACTION_CREATE_POST:
         // create a post
-        console.log(`Sounds like you wanna create a post`);
+        const id = generateId();
+        console.log(`the new id is ${id}`);
+        // console.log(`Sounds like you wanna create a post`);
+        const newState = {
+            ...state,
+            // To use a variable as a key
+            // in an object literal,
+            // wrap the variable in square brackets
+            [id]: action.payload
+        };
+        return newState;
         break;
         default:
             return state;
